fdad54fae90df43ecc08de8e8be9bc0516ac8341
David <david.zhou@hotmail.com.au>
2024-04-30 20:44:42 +1000

    Changed to a RTOS instead of superloop and added a I2C LCD screen for stats and an ISR to turn/off the screen backlight with a button

--- esp32_wifi_monitor.h
+++ esp32_wifi_monitor.h
@@ -1,7 +1,15 @@
 #ifndef ESP_32_WIFI_MONITOR_H
 #define ESP_32_WIFI_MONITOR_H
+#include <arduino.h>
 
+void setupI2cDisplay();
+void setupStatusLED();
 void setupWifiConnection();
-
+void setupRTC();
+void setupSdCard();
+void setup();
+void lcdBacklightToggle();
+void printLocalTime();
+void printAndLog(String str);
 
 #endif /* ESP_32_WIFI_MONITOR_H */
--- esp32_wifi_monitor.ino
+++ esp32_wifi_monitor.ino
@@ -1,36 +1,64 @@
 #include <ESPping.h>
 #include <WiFi.h>
-#include <time.h>
+#include <Wire.h>
 #include <led_strip.h>
+#include <time.h>
+
+#include "LiquidCrystal_I2C.h"
+#include "SD_MMC.h"
 #include "esp32_wifi_monitor.h"
+#include "sd_read_write.h"
 #include "wifi_passwords.h"
 
-#include "sd_read_write.h"
-#include "SD_MMC.h"
-#define SD_MMC_CMD 38 //Please do not modify it.
-#define SD_MMC_CLK 39 //Please do not modify it.
-#define SD_MMC_D0 40 //Please do not modify it.
+// I2C LCD Display
+#define TOGGLE_DISPLAY_BACKLIGHT_BUTTON_PIN 0
+#define SDA 14
+#define SCL 13
+#define LCD_I2C_ADDR 0x3F
+LiquidCrystal_I2C lcd(LCD_I2C_ADDR, 16, 2);
 
+bool lcdBacklight = 0;
+bool doBacklightUpdate = 0;
+
+// SD Card
+#define SD_MMC_CMD 38  // Please do not modify it.
+#define SD_MMC_CLK 39  // Please do not modify it.
+#define SD_MMC_D0 40   // Please do not modify it.
+
+// Onboard RGB LED
 #define SD_STATUS_LED 48
 
+// WIFI User/Pass
 const char *ssidRouter = ROUTER_SSID;
 const char *passwordRouter = ROUTER_PASSWORD;
 
+// Time Server and RTC Settings
 const char *ntpServer = "pool.ntp.org";
-const long gmtOffsetSec = 36000; // GMT +10
+const long gmtOffsetSec = 36000;  // GMT +10
 const int daylightOffsetSec = 0;
 u16_t tick_count = 0;
 
-void printLocalTime() {
-    struct tm timeinfo;
-    if (!getLocalTime(&timeinfo)) {
-        Serial.println("Failed to obtain time");
-        return;
-    }
-    Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+// Used for uptime/downtime statistics
+struct tm currentStatusTime;
+bool netOkay = true;
+
+bool prevPingPassed = true;
+
+void setupI2cDisplay() {
+    Wire.begin(SDA, SCL);
+    lcd.init();
+    lcd.backlight();
+    lcd.setCursor(0, 0);
+    lcd.print("Setting up...");
 }
 
-void setupStatusLED(){
+void setupButtonInterrupt() {
+    pinMode(TOGGLE_DISPLAY_BACKLIGHT_BUTTON_PIN, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(TOGGLE_DISPLAY_BACKLIGHT_BUTTON_PIN),
+                    lcdBacklightToggle, RISING);
+}
+
+void setupStatusLED() {
     pinMode(SD_STATUS_LED, OUTPUT);
     neopixelWrite(SD_STATUS_LED, 255, 255, 255);
 }
@@ -50,10 +78,11 @@ void setupRTC() {
     // Init and get the time
     Serial.println("Configuring RTC with timeserver.");
     configTime(gmtOffsetSec, daylightOffsetSec, ntpServer);
+    getLocalTime(&currentStatusTime);
     printLocalTime();
 }
 
-void setupSdCard(){
+void setupSdCard() {
     SD_MMC.setPins(SD_MMC_CLK, SD_MMC_CMD, SD_MMC_D0);
     if (!SD_MMC.begin("/sdcard", true, true, SDMMC_FREQ_DEFAULT, 5)) {
         Serial.println("Card Mount Failed");
@@ -83,63 +112,162 @@ void setup() {
     while (!Serial)
         ;
     Serial.println("Setup start");
+    setupI2cDisplay();
+    setupButtonInterrupt();
     setupStatusLED();
     setupWifiConnection();
     setupRTC();
     setupSdCard();
+
+    String bootTimeStr;
+    struct tm timeinfo;
+    if (!getLocalTime(&timeinfo)) {
+        bootTimeStr = String("Boot-Time: ??:??");
+    } else {
+        char timeBuffer[64];
+        const char *format = "Boot-Time: %H:%M";
+        strftime(timeBuffer, 64, format, &timeinfo);
+        bootTimeStr = String(timeBuffer);
+    }
+
+    lcd.setCursor(0, 0);
+    lcd.print(bootTimeStr);
+
+
     Serial.println("Setup End");
+
+    /*
+    BaseType_t xTaskCreatePinnedToCore(
+        TaskFunction_t pvTaskCode, const char *const pcName,
+        const uint32_t usStackDepth, void *const pvParameters,
+        UBaseType_t uxPriority, TaskHandle_t *const pvCreatedTask,
+        const BaseType_t xCoreID);
+    */
+    xTaskCreate(lcdDisplayUpdateTask, "Update LCD Display Task", 5000, NULL, 3,
+                NULL);
+
+    xTaskCreate(wifiPingAndLogTask, "Wifi Ping And Log Task", 15000, NULL, 1,
+                NULL);
+    xTaskCreate(sdCardRemountTask, "Remount SD card if needed", 5000, NULL, 2,
+                NULL);
+
+    // xTaskCreatePinnedToCore(lcdDisplayUpdateTask, "Update LCD Display Task",
+    //                         5000, NULL, 1, NULL, 0);
+
+    // xTaskCreatePinnedToCore(wifiPingAndLogTask, "Wifi Ping And Log Task",
+    // 10000,
+    //                         NULL, 2, NULL, 0);
 }
 
-void printAndLog(String str){
+void lcdBacklightToggle() {
+    lcdBacklight = !lcdBacklight;
+    doBacklightUpdate = 1;
+}
+
+void printLocalTime() {
+    struct tm timeinfo;
+    if (!getLocalTime(&timeinfo)) {
+        Serial.println("Failed to obtain time");
+        return;
+    }
+    Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+}
+
+void printAndLog(String str) {
     Serial.print(str);
     appendFile(SD_MMC, "/log.txt", str.c_str());
 }
 
-void loop() {
-    if(!canOpen(SD_MMC, "/log.txt")){
-        neopixelWrite(SD_STATUS_LED, 50, 0, 0);
-        SD_MMC.end();
-        setupSdCard();
-    }
+void loop() { delay(1000); }
 
-    String timeStr;
-    struct tm timeinfo;
-    if (!getLocalTime(&timeinfo)) {
-        timeStr = String("Unknown Time: ");
-    }else{
-        char timeBuffer[64];
-        const char *format = "%Y-%m-%d %H:%M:%S: ";
-        strftime(timeBuffer, 64, format, &timeinfo);
-        timeStr = String(timeBuffer);
-    }
+void lcdDisplayUpdateTask(void *parameter) {
+    for (;;) {
+        if (doBacklightUpdate) {
+            lcd.setBacklight(lcdBacklight);
+            doBacklightUpdate = 0;
+        }
+        time_t currTime;
+        time(&currTime);
+        double diffSeconds = difftime(currTime, mktime(&currentStatusTime));
+        int hours = diffSeconds/3600;
+        int minutes = diffSeconds/60;
+        int seconds = ((int) diffSeconds)%60;
 
-    boolean success = false;
-    if (WiFi.status()) {
-        IPAddress googleDns(8, 8, 8, 8);
-        success = Ping.ping(googleDns);
-        if (success) {
-            printAndLog(timeStr + String(Ping.averageTime()) + String("ms\n"));
-            neopixelWrite(SD_STATUS_LED, 0, 50, 0);
+
+        char timeStatString[100];
+        if(netOkay){
+            sprintf(timeStatString, "UP: %02d:%02d:%02d", hours, minutes, seconds);
+        }else{
+            sprintf(timeStatString, "DOWN: %02d:%02d:%02d", hours, minutes, seconds);
+        }
+        lcd.setCursor(0, 1);
+        lcd.print(timeStatString);
+        vTaskDelay(200);
+    }
+}
+
+void sdCardRemountTask(void *parameter) {
+    for (;;) {
+        if (!canOpen(SD_MMC, "/log.txt")) {
+            neopixelWrite(SD_STATUS_LED, 50, 0, 0);
+            SD_MMC.end();
+            setupSdCard();
+        }
+        vTaskDelay(1000);
+    }
+}
+
+void wifiPingAndLogTask(void *parameter) {
+    for (;;) {
+        String timeStr;
+        struct tm timeinfo;
+        if (!getLocalTime(&timeinfo)) {
+            timeStr = String("Unknown Time: ");
         } else {
-            printAndLog(timeStr + String("FAILED\n"));
-            neopixelWrite(SD_STATUS_LED, 0, 0, 50);
+            char timeBuffer[64];
+            const char *format = "%Y-%m-%d %H:%M:%S: ";
+            strftime(timeBuffer, 64, format, &timeinfo);
+            timeStr = String(timeBuffer);
         }
-    } else {
-        // Serial.println("Wifi not connected. Attempting to reconnect!");
-        printAndLog(timeStr + String("Wifi not connected. Attempting to reconnect!"));
-        WiFi.begin(ssidRouter, passwordRouter);
-        Serial.println(String("Connecting to ") + ssidRouter);
-        while (WiFi.status() != WL_CONNECTED) {
-            Serial.print(".");
+
+        boolean success = false;
+        if (WiFi.status()) {
+            IPAddress googleDns(8, 8, 8, 8);
+            success = Ping.ping(googleDns);
+            if (success) {
+                printAndLog(timeStr + String(Ping.averageTime()) +
+                            String("ms\n"));
+                neopixelWrite(SD_STATUS_LED, 0, 50, 0);
+            } else {
+                printAndLog(timeStr + String("FAILED\n"));
+                neopixelWrite(SD_STATUS_LED, 0, 0, 50);
+            }
+        } else {
+            // Serial.println("Wifi not connected. Attempting to reconnect!");
+            printAndLog(timeStr +
+                        String("Wifi not connected. Attempting to reconnect!"));
+            WiFi.begin(ssidRouter, passwordRouter);
+            Serial.println(String("Connecting to ") + ssidRouter);
+            while (WiFi.status() != WL_CONNECTED) {
+                Serial.print(".");
+            }
         }
+        if (tick_count >= 360) {
+            if (success) {
+                setupRTC();
+                tick_count = 0;
+            }
+        } else {
+            tick_count++;
+        }
+        if(!success && !prevPingPassed && netOkay){
+            getLocalTime(&currentStatusTime);
+            netOkay = false;
+        }else if(success && !netOkay){
+            getLocalTime(&currentStatusTime);
+            netOkay = true;
+        }
+        prevPingPassed = success;
+        vTaskDelay(5000);
     }
-    if(tick_count >= 360){
-        if(success){
-            setupRTC();
-            tick_count = 0;
-        }
-    }else{
-        tick_count++;
-    }
-    delay(5000);
 }
